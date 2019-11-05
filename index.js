ec994747194d92be95b653ceac5dd51d37473a43
renso3x <romeo.enso93@gmail.com>
2019-11-05 18:43:17 +0800

    added transaction logs

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
+const { ApolloServer } = require("apollo-server-express");
+
+var winston = require("./config/winston");
 
 const typeDefs = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
-
-const { ApolloServer, gql } = require("apollo-server-express");
-
 const PORT = 4000;
 
 const app = express();
 
+app.use(morgan("combined"));
+app.use(morgan("combined", { stream: winston.stream }));
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.applyMiddleware({ app });
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 app.listen({ port: PORT }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
