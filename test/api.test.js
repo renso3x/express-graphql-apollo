65bce25cb1c039fb2880b3e598b2b2a23fe7a0b1
Romeo <romeo.enso93@gmail.com>
2019-11-07 10:10:33 +0800

    stub api

--- test/api.test.js
+++ test/api.test.js
@@ -7,8 +7,6 @@ const should = chai.should();
 
 const api = require('./fixtures/api.json');
 
-const base = `http://localhost:4000`;
-
 describe('GraphQL', () => {
   beforeEach(() => {
     this.post = sinon.stub(request, 'post');
@@ -18,7 +16,7 @@ describe('GraphQL', () => {
     request.post.restore();
   });
 
-  it.only('Retrieve an Owner and their Pets', done => {
+  it('Retrieve an Owner and their Pets', done => {
     const obj = api.myPets.success;
     const query =
       '{myPets(userId: 2) { id name breed { name } owner { name} }}';
@@ -35,59 +33,79 @@ describe('GraphQL', () => {
     });
   });
 
-  // it('List of Owners', done => {
-  //   request
-  //     .post('/graphql')
-  //     .send({
-  //       query: '{ owners { id name email } }'
-  //     })
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       const owner = res.body.data.owners[0];
-  //       if (err) return done(err);
-  //       expect(owner).to.have.property('id');
-  //       expect(owner).to.have.property('name');
-  //       expect(owner).to.have.property('email');
-  //       done();
-  //     });
-  // });
+  it('List of Owners', done => {
+    const obj = api.owners.success;
+    const query = '{ owners { id name email } }';
 
-  // it('Create a pet', done => {
-  //   request
-  //     .post('/graphql')
-  //     .send({
-  //       query: `mutation{ createPet(name:"Test" age: 2 colour: "Red" breed: 1 owner: 2) { success message pet { name breed { name } }	} }`
-  //     })
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       const pet = res.body.data.createPet;
-  //       if (err) return done(err);
-  //       expect(pet).to.have.property('success');
-  //       expect(pet).to.have.property('message');
-  //       expect(pet).to.have.property('pet');
-  //       done();
-  //     });
-  // });
+    this.post.yields(null, obj.res, JSON.stringify(obj.body));
 
-  // it('Update a pet', done => {
-  //   request
-  //     .post('/graphql')
-  //     .send({
-  //       query: `mutation{ updatePet( id: 2
-  //         name:"Bae",
-  //         age: 5,
-  //         colour: "green"
-  //         breed: 2
-  //         owner: 1) { success message pet { name breed { name } }	} }`
-  //     })
-  //     .expect(200)
-  //     .end((err, res) => {
-  //       const pet = res.body.data.updatePet;
-  //       if (err) return done(err);
-  //       expect(pet).to.have.property('success');
-  //       expect(pet).to.have.property('message');
-  //       expect(pet).to.have.property('pet');
-  //       done();
-  //     });
-  // });
+    request.post(query, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      res.headers['content-type'].should.contain('application/json');
+      body = JSON.parse(body);
+      body.status.should.eql('success');
+      body.owners[0].should.include.keys(
+        'id',
+        'name',
+        'address',
+        'phone',
+        'email'
+      );
+      done();
+    });
+  });
+
+  it('Create a pet', done => {
+    const obj = api.createPet.success;
+    const query =
+      'mutation{ createPet(name:"Test" age: 2 colour: "Red" breed: 1 owner: 2) { success message pet { name breed { name } }	} }';
+
+    this.post.yields(null, obj.res, JSON.stringify(obj.body));
+
+    request.post(query, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      res.headers['content-type'].should.contain('application/json');
+      body = JSON.parse(body);
+      body.status.should.eql('success');
+      body.message.should.eql('Successfully saved!');
+      body.pet.should.include.keys(
+        'id',
+        'name',
+        'colour',
+        'age',
+        'breed',
+        'owner'
+      );
+      done();
+    });
+  });
+
+  it('Update a pet', done => {
+    const obj = api.updatePet.success;
+    const query = `mutation{ updatePet( id: 2
+          name:"Bae",
+          age: 5,
+          colour: "green"
+          breed: 2
+          owner: 1) { success message pet { name breed { name } }	} }`;
+
+    this.post.yields(null, obj.res, JSON.stringify(obj.body));
+
+    request.post(query, (err, res, body) => {
+      res.statusCode.should.equal(200);
+      res.headers['content-type'].should.contain('application/json');
+      body = JSON.parse(body);
+      body.status.should.eql('success');
+      body.message.should.eql('Successfully saved!');
+      body.pet.should.include.keys(
+        'id',
+        'name',
+        'colour',
+        'age',
+        'breed',
+        'owner'
+      );
+      done();
+    });
+  });
 });
